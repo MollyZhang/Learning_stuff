dd4be5600562256f69b2cbe23c350a66687fe85f
Molly Zhang <mollyzhang@ucsc.edu>
2016-06-28 15:07:40 -0700

    first awk command

---
+++ log.sh
@@ -0,0 +1,8 @@
+awk 'BEGIN{printf "Sr No\tName\tSub\tMarks\n"} {print}' marks.txt 
+#output: 
+#Sr No   Name    Sub Marks
+#1)  Amit    Physics  80
+#2)  Rahul   Maths    90
+#3)  Shyam   Biology  87
+#4)  Kedar   English  85
+#5)  Hari    History  89
