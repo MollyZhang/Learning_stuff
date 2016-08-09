b4b7c20526535aea44bd4ccdcf40b0af473a4d11
Molly Zhang <mollyzhang@ucsc.edu>
2016-08-09 12:12:24 -0700

    add import package example

---
+++ java_learning/package/Boss.java
@@ -0,0 +1,11 @@
+import animals.*;
+public class Boss
+{
+   public static void main(String[] args)
+   {
+    //    animals.Animal myAnimal = new animals.Animal();
+    //    myAnimal.eat();
+        Animal myAnimal = new Animal();
+        myAnimal.eat();
+    }
+}
---
+++ java_learning/package/animals/Animal.java
@@ -0,0 +1,7 @@
+/* File name : Animal.java */
+package animals;
+public class Animal {
+   public void eat() {
+        System.out.println("hey");           
+   };
+}
