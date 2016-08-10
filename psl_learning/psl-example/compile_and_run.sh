088f410948ab101b5752106a455a364da730b8cd
Molly Zhang <mollyzhang@ucsc.edu>
2016-08-10 11:02:30 -0700

    edit compliea_and_run script so it runs the first argument

--- psl_learning/psl-example/compile_and_run.sh
+++ psl_learning/psl-example/compile_and_run.sh
@@ -2,4 +2,4 @@
 mvn -q compile
 mvn -q dependency:build-classpath -Dmdep.outputFile=classpath.out
 echo "====================== mighty result seperator ======================="
-java -cp ./target/classes:`cat classpath.out` edu.umd.cs.example.MollyPracticeExample
+java -cp ./target/classes:`cat classpath.out` edu.umd.cs.example.$1
