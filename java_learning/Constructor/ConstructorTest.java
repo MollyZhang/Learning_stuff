5530e58a97eb0b21283bcc23ebd902fa435367b0
Molly Zhang <mollyzhang@ucsc.edu>
2016-08-09 18:02:59 -0700

    re-organize java learning folder into topic specific

--- java_learning/Constructor/ConstructorTest.java
+++ java_learning/Constructor/ConstructorTest.java
@@ -9,9 +9,8 @@ public class ConstructorTest {
     public static void main(String args[]) {
         ConstructorTest ct = new ConstructorTest(5);
         System.out.println(ct.x);
-//        following is bad
+//        following is bad, because it doens't give the constructor proper arguments
 //        ConstructorTest ct1 = new ConstructorTest();
 //        System.out.println(ct.x);
-        
     }
 }
--- java_learning/FirstProgrammeWithPackage.java
+++
@@ -1,7 +0,0 @@
-package com.jbt;
- 
-public class FirstProgrammeWithPackage {
-   public static void main(String args[]) {
-   System.out.println("Hello JBT!");
-  }
-}
