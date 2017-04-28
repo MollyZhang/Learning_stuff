92db6d375c432b1f46f4b6e024c0b781c5f1c452
Molly Zhang <mollyzhang@ucsc.edu>
2017-04-27 17:48:44 -0700

    confirmed that all path (read or write) need to be absolute, can't even use os.path.abspath("./") because it's still relative

--- linux_learning/cron/hello.py
+++ linux_learning/cron/hello.py
@@ -1,7 +1,9 @@
 import datetime
-import os
-path = os.path.abspath("./")
+path = "/Users/Molly/Desktop/Learning_stuff/linux_learning/cron"
 with open("{0}/hello_from_{1}".format(path, datetime.datetime.now()), "w") as f:
     f.write("hello")
     print("hello")
 f.close()
+
+f1 = open("{0}/read_test.txt".format(path), "r")
+print(f1.read())
