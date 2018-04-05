725c706cd5b8dea4895ab71a9a6757021f8c1f25
Molly <mollyzhang@ucsc.edu>
2018-04-04 22:42:34 -0700

    add starmap example for function with multiple arguments

--- python_learning/multiprocessing/medium_mp.py
+++ python_learning/multiprocessing/medium_mp.py
@@ -5,8 +5,14 @@ def f(*args):
     return sum([i for i in args[0]])
 
 
+def g(*args):
+    print(args)
+    return sum([i for i in args])
+
+
 p = multiprocessing.Pool(2)
 print(p.map(f, [[1,2,3],[2,2],[10,20,30]]))
+print(p.starmap(g, [[1,2,3],[2,2],[10,20,30]]))
 
 
 p.terminate()
